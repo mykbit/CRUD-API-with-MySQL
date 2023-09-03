808aaf5ab45e85d6f5e8d1663651a1620b25d17c
mykbit <mbitiutskyy@gmail.com>
2023-09-03 21:48:26 +0100

    Fix routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,7 +13,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/vinyl/view/:id", app.vinylView)
 	router.HandlerFunc(http.MethodGet, "/vinyl/add", app.vinylCreate)
 	router.HandlerFunc(http.MethodPost, "/vinyl/add", app.vinylCreatePost)
-	router.HandlerFunc(http.MethodPut, "/vinyl/edit", app.vinylUpdate)
+	router.HandlerFunc(http.MethodGet, "/vinyl/edit/:id", app.vinylUpdate)
+	router.HandlerFunc(http.MethodPost, "/vinyl/edit/:id", app.vinylUpdatePost)
 	router.HandlerFunc(http.MethodDelete, "/vinyl/:id", app.vinylDelete)
 
 	return app.logRequest(secureHeaders(router))
